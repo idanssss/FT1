038aa165e9c02b104fd003b7e32ba4e02370a938
TrimericDragon7 <36777029+idanssss@users.noreply.github.com>
2022-09-12 16:40:13 +0300

    initial commit

---
+++ FT1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FT1
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
